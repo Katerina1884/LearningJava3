78b706f075ad4f467fda733cd651f8e9a9b3be48
Ekaterina Sushkova <sushkovakeit@yandex.ru>
2023-08-20 12:52:50 +0300

    add task Rubles

---
+++ Rubles.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Rubles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму");
+        int summa = scanner.nextInt();
+        int x = summa%10;
+        if (summa%100>=11&&summa%100<=14)
+        {
+            System.out.println(summa+ " рублей");
+        }
+        else if(x == 1){
+            System.out.println(summa + " рубль");
+        }
+        else if(x == 2 || x==3 || x ==4){
+            System.out.println(summa + " рубля");
+        }
+        else if(x<=9 && x>=5 || x ==0) {
+            System.out.println(summa + " рублей");
+        }
+
+
+    }
+}
